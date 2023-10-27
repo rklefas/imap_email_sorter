34521a74497db59472d93e8c505aaab58c37e7de
Ryan Klefas <ryan.klefas@gmail.com>
2023-10-26 21:13:49 -0400

    Create an appropriately named mailbox and move an email to it

--- move-emails.py
+++ move-emails.py
@@ -19,52 +19,85 @@ def getfrom(fromhead):
     return frommer
 
 
+def rearrangefrom(frommer):
+    temp = frommer.replace('@', '.')
+    parts = temp.split('.')
+    parts.reverse()
+    temp = '.'.join(parts)
+    return temp
 
+
+def createfolder(FOLDERS):
+
+    pack = ''
+
+    for FOLDER in FOLDERS:
+        pack = pack + '/' + FOLDER
+        pack = pack.strip('/')
+        
+        print('Check for folder:', pack)
+        
+        try:
+            
+            resp_code, mail_count = imap_ssl.select(mailbox='"'+pack+'"')
+            
+            if int(mail_count[0].decode()) >= 0:
+                print("Fetch Mail Count:        {}".format(mail_count[0].decode()))
+
+        except Exception as e:
+        
+            if input('Do you want to create this folder? ') == 'y':
+                imap_ssl.create('"'+pack+'"')
+            else:
+                return False
+        
+    resp_code, mail_count = imap_ssl.select(mailbox='"INBOX"')
+
+    return True
 
 
 configs = json.load(open('./config.json', 'r'))
 
-v_host = configs['host']
-v_user = configs['user']
-v_pass = configs['pass']
-
 ################ IMAP SSL ##############################
 
-with imaplib.IMAP4_SSL(host=v_host, port=imaplib.IMAP4_SSL_PORT) as imap_ssl:
+with imaplib.IMAP4_SSL(host=configs['host'], port=imaplib.IMAP4_SSL_PORT) as imap_ssl:
 
     ############### Login to Mailbox ######################
-    print("Logging into mailbox:   ", v_host)
-    resp_code, response = imap_ssl.login(v_user, v_pass)
+    
+    print("Logging into mailbox:   ", configs['host'])
+    resp_code, response = imap_ssl.login(configs['user'], configs['pass'])
 
     print("Login Result:            {}".format(resp_code))
-#    print("Response:                {}".format(response[0].decode()))
 
-    #################### List Directores #####################
-    resp_code, directories = imap_ssl.list()
-
-    print("Fetch Folder List:       {}".format(resp_code))
-
-    resp_code, mail_count = imap_ssl.select(mailbox='"INBOX"', readonly=True)
+    #################### List Emails #####################
+    
+    resp_code, mail_count = imap_ssl.select(mailbox='"INBOX"')
+    print("Fetch Inbox Count:       {}".format(mail_count[0].decode()))
+    
     typ, data = imap_ssl.search(None, 'ALL')
-    for num in data[0].split():
+    for num in data[0].decode().split():
         
-        print('Message \n\n%s\n' % (num))
+        print('Message # %s\n' % (num))
 
         typ, fromX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (FROM)])')
-        print(fromX[0][1].strip())
+        print(fromX[0][1].decode().strip())
         
         typ, dateX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (DATE)])')
-        print(dateX[0][1].strip())
+        print(dateX[0][1].decode().strip())
         
         typ, subjectX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (SUBJECT)])')
-        print(subjectX[0][1].strip())
+        print(subjectX[0][1].decode().strip())
         
-        FOLDER = 'TESTINGBOX/' + getfrom(fromX[0][1].decode()) + '/' + getyear(dateX[0][1].decode())
-        
-        print('Will create folder:', FOLDER)
-        
-        imap_ssl.create('"'+FOLDER+'"')
-
+        FOLDERSTACK = ["TESTINGBOX"]
+        FOLDERSTACK.append(rearrangefrom(getfrom(fromX[0][1].decode())))
+        FOLDERSTACK.append(getyear(dateX[0][1].decode()))
+                
+        if createfolder(FOLDERSTACK):
+            imap_ssl.copy(num, '/'.join(FOLDERSTACK))
+            imap_ssl.store(num, '+FLAGS', '\\Deleted')
+            imap_ssl.expunge()
+        else:
+            print('Skipping email')
 
         break
     ############# Close Selected Mailbox #######################
