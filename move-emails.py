4ff4cfa8e95b5014bc25c66e58c2dada5f3946a1
Ryan Klefas <ryan.klefas@gmail.com>
2023-10-30 18:58:45 -0400

    Rearrange inbox search and add functions

--- move-emails.py
+++ move-emails.py
@@ -27,33 +27,65 @@ def rearrangefrom(frommer):
     return temp
 
 
-def createfolder(FOLDERS):
 
+def determinefolder(num):
+
+    typ, fromX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (FROM)])')
+    typ, dateX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (DATE)])')
+    
+    FOLDERSTACK = ["AUTOSORT"]
+    FOLDERSTACK.append(rearrangefrom(getfrom(fromX[0][1].decode())))
+    FOLDERSTACK.append(getyear(dateX[0][1].decode()))
+    
+    return FOLDERSTACK
+
+
+def createfolder(num):
+
+    FOLDERSTACK = determinefolder(num)
+    
     pack = ''
 
-    for FOLDER in FOLDERS:
+    for FOLDER in FOLDERSTACK:
         pack = pack + '/' + FOLDER
         pack = pack.strip('/')
         
         print('Check for folder:', pack)
         
-        try:
-            
-            resp_code, mail_count = imap_ssl.select(mailbox='"'+pack+'"')
-            
-            if int(mail_count[0].decode()) >= 0:
-                print("Fetch Mail Count:        {}".format(mail_count[0].decode()))
+        if input('  Do you want to create this folder? ').lower().strip() == 'y':
+            imap_ssl.create('"'+pack+'"')
+        else:
+            return ['AUTOREVIEW']
+        
+    return FOLDERSTACK
+    
 
-        except Exception as e:
-        
-            if input('Do you want to create this folder? ') == 'y':
-                imap_ssl.create('"'+pack+'"')
-            else:
-                return False
-        
-    resp_code, mail_count = imap_ssl.select(mailbox='"INBOX"')
+
+
+def moveemail(FOLDERSTACK, num):
 
     return True
+
+    ROOTFOLDER = '/'.join(FOLDERSTACK)
+    
+    print('Moving email to:', ROOTFOLDER)
+    
+    imap_ssl.copy(num, ROOTFOLDER)
+    imap_ssl.store(num, '+FLAGS', '\\Deleted')
+
+    return True
+
+
+def showemail(num):
+    typ, fromX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (FROM)])')
+    print(fromX[0][1].decode().strip())
+    
+    typ, dateX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (DATE)])')
+    print(dateX[0][1].decode().strip())
+    
+    typ, subjectX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (SUBJECT)])')
+    print(subjectX[0][1].decode().strip())
+
 
 
 configs = json.load(open('./config.json', 'r'))
@@ -74,31 +106,45 @@ with imaplib.IMAP4_SSL(host=configs['host'], port=imaplib.IMAP4_SSL_PORT) as ima
     resp_code, mail_count = imap_ssl.select(mailbox='"INBOX"')
     print("Fetch Inbox Count:       {}".format(mail_count[0].decode()))
     
-    typ, data = imap_ssl.search(None, 'ALL')
+    
+    peekEmail = input('How many emails to sort? ')
+    
+    
+    
+    showemail(peekEmail)
+    
+    typ, fromX = imap_ssl.fetch(peekEmail, '(RFC822.SIZE BODY[HEADER.FIELDS (FROM)])')
+    print(fromX[0][1].decode().strip())
+    
+    typ, dateX = imap_ssl.fetch(peekEmail, '(RFC822.SIZE BODY[HEADER.FIELDS (DATE)])')
+    yearX = getyear(dateX[0][1].decode())
+
+
+    typ, data = imap_ssl.search(None, '(SINCE "01-Jan-'+yearX+'" BEFORE "31-Dec-'+yearX+'" FROM "'+getfrom(fromX[0][1].decode().strip())+'")')
+    
+    print("Emails searched and found:       {}".format(len(data[0].decode().split())))
+
+    
     for num in data[0].decode().split():
+    
         
-        print('Message # %s\n' % (num))
+        print('\nMessage # %s' % (num))
 
-        typ, fromX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (FROM)])')
-        print(fromX[0][1].decode().strip())
+        showemail(num)
         
-        typ, dateX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (DATE)])')
-        print(dateX[0][1].decode().strip())
-        
-        typ, subjectX = imap_ssl.fetch(num, '(RFC822.SIZE BODY[HEADER.FIELDS (SUBJECT)])')
-        print(subjectX[0][1].decode().strip())
-        
-        FOLDERSTACK = ["TESTINGBOX"]
-        FOLDERSTACK.append(rearrangefrom(getfrom(fromX[0][1].decode())))
-        FOLDERSTACK.append(getyear(dateX[0][1].decode()))
-                
-        if createfolder(FOLDERSTACK):
-            imap_ssl.copy(num, '/'.join(FOLDERSTACK))
-            imap_ssl.store(num, '+FLAGS', '\\Deleted')
-            imap_ssl.expunge()
-        else:
-            print('Skipping email')
+        FOLDERSTACK = determinefolder(num)
 
-        break
+       
+        try:
+        
+            moveemail(FOLDERSTACK, num)
+    
+        except Exception as e:
+        
+            print('  Failed to move email!')
+            createfolder(num)
+            moveemail(FOLDERSTACK, num)
+
+            
     ############# Close Selected Mailbox #######################
     imap_ssl.close()
