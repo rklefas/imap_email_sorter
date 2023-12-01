63898857de9d093e18ef50c199443bf5777e8081
Ryan Klefas <ryan.klefas@gmail.com>
2023-12-01 17:11:17 -0500

    Add two new modes to the script for deleting empty folders and bulk moving emails to inbox

--- move-emails.py
+++ move-emails.py
@@ -105,6 +105,39 @@ def println(key, value):
     do_log(key + ' ' + value)
     timeX = datetime.now().strftime("%H:%M:%S ")
     print(timeX, key, ':           ', value)
+    
+# ---------------
+
+def deletefolder(server, folder, status):
+
+    if '/' in folder.name:
+        if '\\HasNoChildren' in folder.flags and status.get('MESSAGES') == 0:
+            if spokeninputtimeout(folder.name + ' has no children folders and no emails.  Delete? ', 'y') == 'y':
+                println('DELETE', folder.name)
+                server.folder.delete(folder.name)
+    
+# ---------------
+
+def moveemails(server, FULLPATH, listings):
+
+    pack = ''
+    
+    for xid in listings:
+    
+        pack = pack + xid + ','
+        
+        if pack.count(',') == 10:
+            server.move(pack.strip(','), FULLPATH)
+            println('  Moving emails', pack)
+            pack = ''
+    
+    if pack != '':
+        server.move(pack.strip(','), FULLPATH)
+        println('  Moving emails', pack)
+
+    stat = server.folder.status(FULLPATH)
+    print(stat)
+
 
 # ---------------
 # ---------------
@@ -114,9 +147,81 @@ min_timeout = 2
 max_timeout = 60
 dynamic_timeout = max_timeout
 
+
+mode = spokeninput('Which mode? Delete / Move / Sort (D M S) ').upper()
+server = imap_tools.MailBox(configs['host']).login(configs['user'], configs['pass'])
+
+
+    
+
+if mode == 'D':
+
+    for cycle in range(1, 3):
+
+        folders = list(server.folder.list())
+        speakline('Cycle ' + str(cycle) + ' folders to scan', str(len(folders)))
+
+        for f in folders:
+                    
+            try:
+                
+                print(f.name)  # FolderInfo(name='INBOX|cats', delim='|', flags=('\\Unmarked', '\\HasChildren'))
+                stat = server.folder.status(f.name)
+                
+                deletefolder(server, f, stat)
+
+            except Exception as e:
+                speakline('Failed to stat folder for deletion', str(e))
+
+        
+            
+elif mode == 'M':
+
+    while True:
+
+        go = spokeninput('Folder filter: ')
+        go = '*' + go + '*'
+        
+        folders = list(server.folder.list(search_args=go))        
+        speakline('Folders to scan', str(len(folders)))
+        
+        for f in folders:
+            stat = server.folder.status(f.name)
+            
+            if '\\HasNoChildren' in f.flags and stat.get('MESSAGES') > 0:
+                println(f.name, 'has no children folders and ' + str(stat.get('MESSAGES')) + ' emails')
+            elif '\\HasNoChildren' in f.flags and stat.get('MESSAGES') == 0:
+                deletefolder(server, f, stat)
+            else:
+                println(f.name, '')
+                    
+        if spokeninput('Empty all of these folders? ') == 'y':
+
+            destinationfolder = spokeninput('Which folder to put in? ')
+            
+            stat = server.folder.status(destinationfolder)
+            print(stat)
+
+
+            for f in folders:
+                println(f.name, '')
+                server.folder.set(f.name)
+                
+                preview = list(server.fetch(bulk=True))
+                EMAILLIST = []
+
+                for index, msg in enumerate(preview):
+                    EMAILLIST.append(msg.uid)
+
+
+                moveemails(server, destinationfolder, EMAILLIST)
+            
+
+elif mode == 'S':
+
 ############### Login to Mailbox ######################
 
-with imap_tools.MailBox(configs['host']).login(configs['user'], configs['pass']) as server:
+    server = imap_tools.MailBox(configs['host']).login(configs['user'], configs['pass'])
 
     println("Logged into mailbox", configs['host'])
 
@@ -192,20 +297,7 @@ with imap_tools.MailBox(configs['host']).login(configs['user'], configs['pass'])
         
         try:
             
-            pack = ''
-            
-            for xid in EMAILLIST:
-            
-                pack = pack + xid + ','
-                
-                if pack.count(',') == 10:
-                    server.move(pack.strip(','), FULLPATH)
-                    println('  Moving emails', pack)
-                    pack = ''
-            
-            if pack != '':
-                server.move(pack.strip(','), FULLPATH)
-                println('  Moving emails', pack)
+            moveemails(server, FULLPATH, EMAILLIST)
             
             counting = len(EMAILLIST)
             runtimecount = runtimecount + counting
